31dcbf81329415cbcb92d5b466058bed0524fcad
Wendly Saintil <wendlysaintil@gmail.com>
2022-04-06 15:25:06 -0400

    Add chezmoi install and vimrc

--- install.sh
+++ install.sh
@@ -1,12 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
-cd ~
+# https://github.com/chezmoi/dotfiles/blob/master/install.sh
 
-cp ~/dotfiles/vim/.vimrc ~/.vimrc
-echo "Replaced vimrc"
+set -o errexit
 
-cp ~/dotfiles/zsh/.zshrc ~/.zshrc
-echo "Replaced vimrc"
+if ! command -v chezmoi >/dev/null 2>&1; then
+  bin_dir="${HOME}/.local/bin"
+  chezmoi="${bin_dir}/chezmoi"
+  if command -v curl >/dev/null 2>&1; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif command -v wget >/dev/null 2>&1; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
 
-rm -rf ~/dotfiles
-echo "Done"
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+# Print out the following commands before executing
+set -o xtrace
+
+echo Installing Pathogen for vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo Installing emmet for vim
+git clone https://github.com/mattn/emmet-vim.git ~/.vim/bundle/emmet-vim
+
+echo Installing nerdtree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=${script_dir}"
